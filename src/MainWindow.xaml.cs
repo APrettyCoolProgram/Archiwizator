89478eb401e1b2a7952963f650a3a7f2b0eac01d
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2020-12-24 16:53:54 -0500

    v0.2.20359.2153

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿/* PROJECT: Archiwizator (https://github.com/aprettycoolprogram/Archiwizator)
+ *    FILE: Archiwizator.MainWindow.xaml.cs
+ * UPDATED: 12-24-2020-1:30 PM
+ * LICENSE: Apache v2 (https://apache.org/licenses/LICENSE-2.0)
+ *          Copyright 2020 A Pretty Cool Program All rights reserved
+ */
+
+using System.Reflection;
+using System.Windows;
+using Du;
+
+namespace Archiwizator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        public void Setup()
+        {
+        }
+    }
+}

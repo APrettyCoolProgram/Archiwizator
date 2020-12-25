687e252011350163a95e1ed01686b825f61978ba
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2020-12-24 19:36:19 -0500

    v0.3.20360.0016

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -5,7 +5,7 @@
  *          Copyright 2020 A Pretty Cool Program All rights reserved
  */
 
-using System.Reflection;
+using System.IO;
 using System.Windows;
 using Du;
 
@@ -16,11 +16,73 @@ namespace Archiwizator
         public MainWindow()
         {
             InitializeComponent();
+
             Setup();
         }
 
         public void Setup()
         {
+            SetupControls();
         }
+
+        private void SetupControls()
+        {
+            // Compression level options.
+            cmbxCompressionLevel.Items.Add("Store");
+            cmbxCompressionLevel.Items.Add("Fastest");
+            cmbxCompressionLevel.Items.Add("Fast");
+            cmbxCompressionLevel.Items.Add("Normal");
+            cmbxCompressionLevel.Items.Add("Maximum");
+            cmbxCompressionLevel.Items.Add("Ultra");
+            cmbxCompressionLevel.SelectedIndex = 3;
+
+            // Folder choice.
+            txbxFolderChoice.Text = "";
+
+            // Delete original folder options.
+            ckbxDeleteOriginalFolder.IsEnabled = false;
+            ckbxDeleteOriginalFolder.IsChecked = false;
+
+            // Prepend date stamp options.
+            ckbxPrependDateStamp.IsChecked = false;
+
+            // Delete specific files options.
+            //ckbxDeleteSpecificFilesBeforeCompressing.IsEnabled = false;
+            ckbxDeleteSpecificDirectoriesBeforeCompressing.IsChecked = false;
+            txbxSpecificDirectoriesToDeleteBeforeCompressing.Text    = "*temp*, *Temp*";
+
+            // Uncompress before compressing options.
+            ckbxUncompressFilesBeforeCompressing.IsEnabled = false;
+            ckbxUncompressFilesBeforeCompressing.IsChecked = false;
+            cmbxLevelsOfFilesToUncompressBeforeCompressing.Items.Add("1");
+            cmbxLevelsOfFilesToUncompressBeforeCompressing.Items.Add("2");
+            cmbxLevelsOfFilesToUncompressBeforeCompressing.Items.Add("3");
+
+            // Start button.
+            btnStart.IsEnabled = false;
+        }
+
+        private void ModifyStartButtonState()
+        {
+            if(txbxFolderChoice.Text != "")
+            {
+                btnStart.IsEnabled = Directory.Exists(txbxFolderChoice.Text);
+            }
+        }
+
+        private void ChooseFolder()
+        {
+            txbxFolderChoice.Text = DuFolderDialog.GetFolderPath();
+        }
+
+        private void Start()
+        {
+            SevenZip.Create(txbxFolderChoice.Text, (string)cmbxCompressionLevel.SelectedItem, lblUpdateUser, (bool)ckbxDeleteOriginalFolder.IsChecked, (bool)ckbxPrependDateStamp.IsChecked, txbxSpecificDirectoriesToDeleteBeforeCompressing.Text);
+        }
+
+        // EVENT HANDLERS
+        private void btnFolderChoice_Click(object sender, RoutedEventArgs e) => ChooseFolder();
+        private void txbxFolderChoice_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) => ModifyStartButtonState();
+        private void btnStart_Click(object sender, RoutedEventArgs e) => Start();
     }
 }
---
+++ src/SevenZip.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Du;
+
+
+namespace Archiwizator
+{
+    public class SevenZip
+    {
+        private static readonly Action EmptyDelegate = delegate { };
+
+        /// <summary>
+        /// Refresh the progress label.
+        /// </summary>
+        /// <param name="label">The progress label.</param>
+        public static void Refresh(Label label)
+        {
+            label.Dispatcher.Invoke(DispatcherPriority.Render, EmptyDelegate);
+        }
+
+        /// <summary>
+        /// Create an archive.
+        /// </summary>
+        /// <param name="folderPath">Path to the folder to archive.</param>
+        /// <param name="compressLevel">The compression level.</param>
+        /// <param name="lblProgress">The progress indicator</param>
+        /// <param name="deleteOriginalFolder">Determines if the original folder should be deleted after archiving.</param>
+        public static void Create(string folderPath, string compressLevel, Label lblProgress, bool deleteOriginalFolder, bool prependDateStamp, string directoriesToDelete)
+        {
+            var subDirectories= DuDirectory.GetSubDirectoryNames(folderPath);
+
+            var compressionLevel = compressLevel switch
+            {
+                "Fastest" => "-mx1",
+                "Fast"    => "-mx3",
+                "Normal"  => "-mx5",
+                "Maximum" => "-mx7",
+                "Ultra"   => "-mx9",
+                _         => "-mx0",
+            };
+
+            var sevenZipExecutablePath = DuOperatingSystem.MSWindows.Is64Bit()
+                ? @"./resources/Bin/7Zip/64bit/7za.exe"
+                : @"./resources/Bin/7Zip/32bit/7za.exe";
+
+            var numberOfSubDirectories = subDirectories.Count;
+            var currSubDirectoryCount = 1;
+
+            var dirsToDelete = DuString.SplitAtDelimiter(directoriesToDelete, ',');
+
+            foreach(var subDirectory in subDirectories)
+            {
+                var sourceDirectory = $"{folderPath}{subDirectory}\\";
+                var archiveFilePath = "";
+
+                if(prependDateStamp)
+                {
+                    var dt = DateTime.Now.ToString("yyMMdd");
+
+                    archiveFilePath = $"{folderPath}{subDirectory}-{dt}.7z";
+                }
+                else
+                {
+                    archiveFilePath = $"{folderPath}{subDirectory}.7z";
+                }
+
+                var command = $"a -t7z {compressionLevel} \"{archiveFilePath}\" \"{sourceDirectory}*\"";
+
+                lblProgress.Content = $"PROGRESS: File {currSubDirectoryCount} of {numberOfSubDirectories}: {subDirectory}";
+                Refresh(lblProgress);
+
+                DuCompression.SevenZip.CreateFromDirectory(sevenZipExecutablePath, archiveFilePath, command);
+
+                if(deleteOriginalFolder)
+                {
+                    DuDirectory.Delete(sourceDirectory);
+                }
+
+                currSubDirectoryCount++;
+            }
+
+            lblProgress.Content = $"PROGRESS: COMPLETE!";
+        }
+    }
+}

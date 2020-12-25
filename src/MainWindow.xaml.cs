e40c068d8faaf059cf2937f2e3bf6504608bdda8
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2020-12-25 11:34:42 -0500

    v0.6.20360.1629

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿/* PROJECT: Archiwizator (https://github.com/aprettycoolprogram/Archiwizator)
  *    FILE: Archiwizator.MainWindow.xaml.cs
- * UPDATED: 12-24-2020-1:30 PM
+ * UPDATED: 12-25-2020-9:27 AM
  * LICENSE: Apache v2 (https://apache.org/licenses/LICENSE-2.0)
  *          Copyright 2020 A Pretty Cool Program All rights reserved
  */
@@ -17,17 +17,14 @@ namespace Archiwizator
         {
             InitializeComponent();
 
-            Setup();
+            SetCompressionLevelOptions();
         }
 
-        public void Setup()
+        /// <summary>Setup the compression level options.</summary>
+        /// <remarks>These options mirror the options available in 7-Zip.</remarks>
+        private void SetCompressionLevelOptions()
         {
-            SetupControls();
-        }
-
-        private void SetupControls()
-        {
-            // Compression level options.
+            // TODO: This should eventually be moved to the .xaml file.
             cmbxCompressionLevel.Items.Add("Store");
             cmbxCompressionLevel.Items.Add("Fastest");
             cmbxCompressionLevel.Items.Add("Fast");
@@ -35,33 +32,16 @@ namespace Archiwizator
             cmbxCompressionLevel.Items.Add("Maximum");
             cmbxCompressionLevel.Items.Add("Ultra");
             cmbxCompressionLevel.SelectedIndex = 3;
-
-            // Folder choice.
-            txbxFolderChoice.Text = "";
-
-            // Delete original folder options.
-            ckbxDeleteOriginalFolder.IsEnabled = false;
-            ckbxDeleteOriginalFolder.IsChecked = false;
-
-            // Prepend date stamp options.
-            ckbxPrependDateStamp.IsChecked = false;
-
-            // Delete specific files options.
-            //ckbxDeleteSpecificFilesBeforeCompressing.IsEnabled = false;
-            ckbxDeleteSpecificDirectoriesBeforeCompressing.IsChecked = false;
-            txbxSpecificDirectoriesToDeleteBeforeCompressing.Text = "!Temp";
-
-            // Uncompress before compressing options.
-            //ckbxUncompressFilesBeforeCompressing.IsEnabled = false;
-            ckbxUncompressFilesBeforeCompressing.IsChecked = false;
-            cmbxLevelsOfFilesToUncompressBeforeCompressing.Items.Add("1");
-            cmbxLevelsOfFilesToUncompressBeforeCompressing.Items.Add("2");
-            cmbxLevelsOfFilesToUncompressBeforeCompressing.Items.Add("3");
-
-            // Start button.
-            btnStart.IsEnabled = false;
         }
 
+        /// <summary>Enables/disables the txbxSpecificDirectoriesToDeleteBeforeCompressing control.</summary>
+        private void ModifyTxbxSpecificDirectoriesToDeleteBeforeCompressingState()
+        {
+            txbxSpecificDirectoriesToDeleteBeforeCompressing.IsEnabled = (bool)ckbxDeleteDirectoriesBeforeCompressing.IsChecked;
+
+        }
+
+        /// <summary>Enables/disables the btnStart control.</summary>
         private void ModifyStartButtonState()
         {
             if(txbxFolderChoice.Text != "")
@@ -70,6 +50,7 @@ namespace Archiwizator
             }
         }
 
+        /// <summary>Prompts the user to choose a folder.</summary>
         private void ChooseFolder()
         {
             txbxFolderChoice.Text = DuFolderDialog.GetFolderPath();
@@ -77,12 +58,35 @@ namespace Archiwizator
 
         private void Start()
         {
-            SevenZip.Create(txbxFolderChoice.Text, (string)cmbxCompressionLevel.SelectedItem, lblUpdateUser, (bool)ckbxDeleteOriginalFolder.IsChecked, (bool)ckbxPrependDateStamp.IsChecked, (bool)ckbxDeleteSpecificDirectoriesBeforeCompressing.IsChecked, txbxSpecificDirectoriesToDeleteBeforeCompressing.Text, (bool)ckbxUncompressFilesBeforeCompressing.IsChecked);
+            var sz = new DuSevenZip()
+            {
+                ArchiwizatorArgs = new DuSevenZip.ArchiwizatorArguments()
+                {
+                    PostfixDateStamp                      = (bool)ckbxPrependDateStamp.IsChecked,
+                    RemoveDirectoriesPriorToCompression   = (bool)ckbxDeleteDirectoriesBeforeCompressing.IsChecked,
+                    DirectoriesToRemovePriorToCompression = txbxSpecificDirectoriesToDeleteBeforeCompressing.Text,
+                    UncompressPriorToCompression          = (bool)ckbxUncompressFilesBeforeCompressing.IsChecked
+                },
+                SevenZipArgs = new DuSevenZip.SevenZipArguments()
+                {
+                    Action                      = "a",
+                    SourcePath                  = txbxFolderChoice.Text,
+                    DestinationPath             = "",
+                    CompressionLevel            = (string)cmbxCompressionLevel.SelectedItem,
+                    DeleteFilesAfterCompression = true
+                }
+            };
+
+            //DuSevenZip.Compress(sz);
+
+            SevenZip.Create(sz);
         }
 
         // EVENT HANDLERS
         private void btnFolderChoice_Click(object sender, RoutedEventArgs e) => ChooseFolder();
         private void txbxFolderChoice_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) => ModifyStartButtonState();
         private void btnStart_Click(object sender, RoutedEventArgs e) => Start();
+        private void ckbxDeleteDirectoriesBeforeCompressing_Checked(object sender, RoutedEventArgs e) => ModifyTxbxSpecificDirectoriesToDeleteBeforeCompressingState();
+
     }
 }
--- src/SevenZip.cs
+++ src/SevenZip.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿/* PROJECT: Archiwizator (https://github.com/aprettycoolprogram/Archiwizator)
+ *    FILE: Archiwizator.SevenZip.cs
+ * UPDATED: 12-25-2020-9:27 AM
+ * LICENSE: Apache v2 (https://apache.org/licenses/LICENSE-2.0)
+ *          Copyright 2020 A Pretty Cool Program All rights reserved
+ */
+
+using System;
 using System.IO;
 using System.Windows.Controls;
 using System.Windows.Threading;
 using Du;
-
 
 namespace Archiwizator
 {
@@ -27,99 +33,112 @@ namespace Archiwizator
         /// <param name="compressLevel">The compression level.</param>
         /// <param name="lblProgress">The progress indicator</param>
         /// <param name="deleteOriginalFolder">Determines if the original folder should be deleted after archiving.</param>
-        public static void Create(string folderPath, string compressLevel, Label lblProgress, bool deleteOriginalFolder, bool prependDateStamp, bool deleteSpecificDirectories, string directoriesToDelete, bool uncompressFilesBeforeCompressing)
+        public static void Create(DuSevenZip sz)
         {
-            var subDirectories= DuDirectory.GetSubDirectoryNames(folderPath);
-
-            var compressionLevel = compressLevel switch
-            {
-                "Fastest" => "-mx1",
-                "Fast"    => "-mx3",
-                "Normal"  => "-mx5",
-                "Maximum" => "-mx7",
-                "Ultra"   => "-mx9",
-                _         => "-mx0",
-            };
-
-            var sevenZipExecutablePath = DuOperatingSystem.MSWindows.Is64Bit()
-                ? @"./resources/Bin/7Zip/64bit/7za.exe"
-                : @"./resources/Bin/7Zip/32bit/7za.exe";
+            var subDirectories = DuDirectory.GetSubDirectoryNames(sz.SevenZipArgs.SourcePath);
 
             var numberOfSubDirectories = subDirectories.Count;
             var currSubDirectoryCount = 1;
 
-            var dirsToDelete = DuString.ToListAtDelimiter(directoriesToDelete, ',');
-
             foreach(var subDirectory in subDirectories)
             {
-                var sourceDirectory = $"{folderPath}{subDirectory}\\";
-                var archiveFilePath = "";
-
-                if(deleteSpecificDirectories)
-                {
-                    var subSubDirectories = DuDirectory.GetSubDirectoryNames(sourceDirectory);
-
-                    foreach(var subsub in subSubDirectories)
-                    {
-                        if(directoriesToDelete.Contains(subsub))
-                        {
-                            DuDirectory.Delete($"{sourceDirectory}{subsub}\\");
-                        }
-                    }
-                }
-
-                if(uncompressFilesBeforeCompressing)
-                {
-                    var files = DuDirectory.GetFileNames(sourceDirectory);
-
-                    foreach(var file in files)
-                    {
-                        var fileExtension = file.Extension.ToLower();
+                var cmd = DuSevenZip.BuildSevenZipCommand(sz);
 
 
-
-
-                        if(fileExtension == ".zip")
-                        {
-                            var fi = Path.GetFileNameWithoutExtension(file.FullName);
-
-                            archiveFilePath = $"{file.FullName}";
-                            var extractCommand = $"x {archiveFilePath} -o{sourceDirectory}{fi}";
-                            DuCompression.SevenZip.ExtractToDirectory(sevenZipExecutablePath, archiveFilePath, extractCommand);
-                        }
-
-
-
-                    }
-                }
-
-                if(prependDateStamp)
-                {
-                    var dt = DateTime.Now.ToString("yyMMdd");
-
-                    archiveFilePath = $"{folderPath}{subDirectory}-{dt}.7z";
-                }
-                else
-                {
-                    archiveFilePath = $"{folderPath}{subDirectory}.7z";
-                }
-
-                var command = $"a {compressionLevel} \"{archiveFilePath}\" \"{sourceDirectory}*\"";
-
-                lblProgress.Content = $"PROGRESS: File {currSubDirectoryCount} of {numberOfSubDirectories}: {subDirectory}";
-                Refresh(lblProgress);
-
-                DuCompression.SevenZip.CreateFromDirectory(sevenZipExecutablePath, archiveFilePath, command);
-
-                if(deleteOriginalFolder)
-                {
-                    DuDirectory.Delete(sourceDirectory);
-                }
-
-                currSubDirectoryCount++;
             }
 
-            lblProgress.Content = $"PROGRESS: COMPLETE!";
+
+
+
+
+
+
+
+
+
+            //var dirsToDelete = DuString.ToListAtDelimiter(directoriesToDelete, ',');
+
+            //foreach(var subDirectory in subDirectories)
+            //{
+            //    var sourceDirectory = $"{folderPath}{subDirectory}\\";
+            //    var archiveFilePath = "";
+
+            //    if(deleteSpecificDirectories)
+            //    {
+            //        var subSubDirectories = DuDirectory.GetSubDirectoryNames(sourceDirectory);
+
+            //        foreach(var subsub in subSubDirectories)
+            //        {
+            //            if(directoriesToDelete.Contains(subsub))
+            //            {
+            //                DuDirectory.Delete($"{sourceDirectory}{subsub}\\");
+            //            }
+            //        }
+            //    }
+
+            //    if(uncompressFilesBeforeCompressing)
+            //    {
+            //        var files = DuDirectory.GetFileNames(sourceDirectory);
+
+            //        foreach(var file in files)
+            //        {
+            //            var fileExtension = file.Extension.ToLower();
+
+
+
+
+            //            if(fileExtension == ".zip")
+            //            {
+            //                var fi = Path.GetFileNameWithoutExtension(file.FullName);
+
+            //                archiveFilePath = $"{file.FullName}";
+            //                var extractCommand = $"x {archiveFilePath} -o{sourceDirectory}{fi}";
+            //                DuSevenZip.ExtractToDirectory(sevenZipExecutablePath, archiveFilePath, extractCommand);
+            //            }
+
+
+
+            //        }
+            //    }
+
+            //    if(prependDateStamp)
+            //    {
+            //        var dt = DateTime.Now.ToString("yyMMdd");
+
+            //        archiveFilePath = $"{folderPath}{subDirectory}-{dt}.7z";
+            //    }
+            //    else
+            //    {
+            //        archiveFilePath = $"{folderPath}{subDirectory}.7z";
+            //    }
+
+            //    var archiveCommand = "";
+
+            //    if(deleteOriginalFolder)
+            //    {
+            //        archiveCommand = $"a {compressionLevel} \"{archiveFilePath}\" \"{sourceDirectory}*\" -sdel";
+            //    }
+            //    else
+            //    {
+            //        archiveCommand = $"a {compressionLevel} \"{archiveFilePath}\" \"{sourceDirectory}*\"";
+            //    }
+
+            //    if(deleteOriginalFolder)
+            //    {
+            //        DuDirectory.Delete(sourceDirectory);
+            //    }
+
+            //    lblProgress.Content = $"PROGRESS: File {currSubDirectoryCount} of {numberOfSubDirectories}: {subDirectory}";
+            //    Refresh(lblProgress);
+
+            //    DuSevenZip.CreateFromDirectory(sevenZipExecutablePath, archiveFilePath, archiveCommand);
+
+
+
+            //    currSubDirectoryCount++;
+            //}
+
+            //lblProgress.Content = $"PROGRESS: COMPLETE!";
         }
     }
 }
